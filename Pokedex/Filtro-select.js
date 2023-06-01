b9052ffdc866773e4cc4ce42f69c574dfdd39316
Jorge Moya <17.jorgemoya@gmail.com>
2023-06-01 18:18:29 -0400

    show info Pokemon

--- Pokedex/Filtro-select.js
+++ Pokedex/Filtro-select.js
@@ -1,5 +1,4 @@
-import peticion from "./searchPokemon.js"
-import showPokemon from "./Pokedex.js";
+import peticion , { createPokemon } from "./Pokedex.js";
 
 export default async function generateOptions (select){
    const options = await peticion(`https://pokeapi.co/api/v2/${select.id}`);
@@ -15,7 +14,6 @@ export default async function generateOptions (select){
 
 export async function filterPokemon(selected){
    pokeTable.innerHTML = "" ;
-
    const result = await peticion(selected.value);
    
    switch (selected.id) {
@@ -27,21 +25,18 @@ export async function filterPokemon(selected){
 };
 
 function filterGeneration({pokemon_species}) {
-
    pokemon_species.forEach(async species =>{
       
       const specie = await peticion(species.url);
-      const variety = specie.varieties.find(e => e.is_default = true);
-      const pokemon = await peticion(variety.pokemon.url);
-      showPokemon(pokemon);
+      createPokemon(specie);
    })
 };
-async function filterType({pokemon}) {
+function filterType({pokemon}) {
    
    pokemon.forEach(async pokemon => {
       
       const result = await peticion(pokemon.pokemon.url);
-      showPokemon(result);
+      createPokemon(result);
       
    })
 };
--- Pokedex/Pokedex.js
+++ Pokedex/Pokedex.js
@@ -1,42 +1,147 @@
 import generateOptions, { filterPokemon } from "./Filtro-select.js";
-import peticion, {pokeInfo}  from "./searchPokemon.js";
 
-//queryselector
+
+export default async function peticion (url) {
+    const response = await fetch(url);  
+    const responseJson = await response.json();
+    
+    return responseJson;
+};
+//Variables
 const pokeTable = document.querySelector('#pokeTable');
 const selects = document.querySelectorAll('.select');
+let pokeArray = []
+let min = 1;
+let max = 12;
+
+getPokemon()
+upEvent()
 
 //eventos
-
-pokeTable.onload = allPokemon()
-
-selects.forEach(select =>{
-
-    select.onload = generateOptions(select)
-
-    select.addEventListener('change', (e)=>{             
-        e.preventDefault();
-        filterPokemon(select);
-    });
+function upEvent (){
     
-});
+    selects.forEach(select =>{
+    
+        select.onclick = generateOptions(select)
+    
+        select.addEventListener('change', (e)=>{             
+            e.preventDefault();
+            filterPokemon(select);
+        });
+        
+    });
+
+    pokeTable.addEventListener('click', (e)=>{
+        const card = e.target
+        if(card.classList.contains('pokeCard'))pokeInfo(card.dataset.info);
+        if(card.classList.length === 0)pokeInfo(card.parentElement.dataset.info);
+
+    })
+    pokeDatos.addEventListener('click', (e)=>{
+        e.preventDefault();
+        if(e.target.classList.contains('btn-close')) showOrHidden();
+    })
+    
+}
+
 
 //funciones
+async function getPokemon(){
+    for( min; min <= max; min++){
+        const pokemon = await peticion(`https://pokeapi.co/api/v2/pokemon-species/${min}/`)
+        cardHTML(await createPokemon(pokemon));
+    }
+}
 
-export default function showPokemon(pokemon) {
-    const pokeCard = `
-    <div class="pokeCard" data-info="${pokemon}"onclick="pokeInfo(this)">
-        <h4># ${pokemon.id.toString().padStart(3,0)}</h4>
-        <img src="${pokemon.sprites.front_default}" >
-        <h1>${pokemon.name}</h1>
-    </div>`
+
+export async function createPokemon(pokemonJson) {
     
-    pokeTable.innerHTML += pokeCard ;
+    const {name,id,generation,flavor_text_entries,varieties} = pokemonJson;
+    const pokemon = varieties.find(e => e.is_default == true)
+    const {sprites,types} = await peticion(pokemon.pokemon.url)
+    const info  = {
+        id,
+        type: types.map(type => type.type.name),
+        name,
+        img: {front: sprites.front_default, other: sprites.other.dream_world.front_default},
+        generation: generation.url,
+        flavors: flavor_text_entries
+    }
+    
+    return info
 };
 
-async function allPokemon( max = 12 ){
+function cardHTML(info){
+    
+    const pokeCard = document.createElement('div');
+    pokeCard.classList.add('pokeCard');
+    pokeCard.dataset.info= JSON.stringify(info);
+    const card = `
+    <h4># ${info.id.toString().padStart(3,0)}</h4>
+    <img src="${info.img.front}" >
+    <h1>${info.name}</h1>
+    `
+    pokeCard.innerHTML = card;
+    pokeArray.push(pokeCard);
+    showCard();
+}
 
-   for(let i = 1; i <= max; i++){
-      const pokemon = await peticion(`https://pokeapi.co/api/v2/pokemon/${i}/`)
-      showPokemon(pokemon);
-   }
+function showCard( ){
+    
+    pokeTable.innerHTML = '';
+    pokeArray.forEach( pokemon =>{
+        pokeTable.appendChild(pokemon)
+    });
+}
+
+async function pokeInfo(pokemon) {
+    const info = JSON.parse(pokemon);
+    const {id,type,name,img,generation,flavors} = info
+    const generacion = await peticion(generation)
+    let descripcion = '';
+
+    flavors.forEach(flavor =>{
+        const {flavor_text , language, version } = flavor;
+        if(language.name === 'es'){
+            if(version.name) descripcion += flavor_text
+        }
+    })
+        
+    showOrHidden();
+    
+    pokeDatos.innerHTML = `
+        <div id="p">
+            <div id="datos">
+                <div id="principal">
+                    <h1>${name}</h1>
+                    <h2>ID: # ${id.toString().padStart(3,0)}</h2>
+                </div>
+                <div id="otros">
+                <h4>Generación: ${generacion.name}</h4>
+                <h4>Región: ${generacion.main_region.name}</h4>
+                <h4>Tipos:${type}</h4>
+                </div>
+                </div>
+                <div id="imagen">
+                <img src="${img.other}">
+                </div>
+        </div>
+        <div id="descripcion">
+        <p>${descripcion}</p>
+        </div>
+        <figure class="btn-close"> 
+            <div id="top"></div>
+            <span id="center">X</span>
+            <div id="bottom"></div>
+        </figure>`;
+        
+};
+
+function showOrHidden(){
+    console.log('si conecta');
+    switch(pokeDatos.style.display){
+        case '': pokeDatos.style.display = 'grid'; break;
+        case 'grid': pokeDatos.style.display = 'none'; break;
+        default : pokeDatos.style.display = 'grid'; break;
+    }
 }
